5123efdccafcf5dff405bf8470987588d5aeeb59
Bunhouth <bunhouth99@gmail.com>
2015-07-23 00:39:10 +0700

    change swipe logic

--- Ionic-Mobile-kid-game/www/js/controllers.js
+++ Ionic-Mobile-kid-game/www/js/controllers.js
@@ -17,39 +17,37 @@ angular.module('kid_game.controllers', [])
   var age = localStorage.getItem("age") != null ? parseInt(localStorage.getItem("age")) : 3;
   var min_number = 60;
   var numbers = age > 3 ? min_number * age : min_number ;
-
+  var minNumber = min_number;
   var menu = Menu.get($stateParams.menuCode);
   $scope.menu = menu;
-  $scope.limit_number = numbers;
   $scope.active_tab = {
     menu: $stateParams.menuCode,
     item: $stateParams.menuDetail
   };
-
+  $scope.number_lists = rangs(0,minNumber)
   $scope.alphabets = alphabet;
-   $scope.next = function() {
+  $scope.next = function() {
     $ionicSlideBoxDelegate.next();
   };
   $scope.previous = function() {
     $ionicSlideBoxDelegate.previous();
   };
 
-  $scope.cardSwipedLeft = function() {
-    $scope.addRandom();
-    // $scope.index_number = parseInt(index) + 1
+  $scope.cardSwipedLeft = function(index) {
+    if(index == minNumber -1 && minNumber <= numbers){
+      minNumber = minNumber * 2;
+      new_number = numbers > minNumber ? minNumber : numbers;
+      $scope.number_lists = rangs(0,new_number)
+      $ionicSlideBoxDelegate.update();
+    }
   }
 
-  $scope.cardSwipedRight = function() {
-    $scope.addRandom();
-    // $scope.index_number = parseInt(index) - 1
-  }
-
-  $scope.range = function(min, max, step){
+  function rangs(min, max, step){
     step = step || 1;
     var input = [];
     for (var i = min; i <= max; i += step) input.push(i);
     return input;
-  };
+  }
 
   $scope.random = ['0','A'];
   $scope.addRandom = function(){
